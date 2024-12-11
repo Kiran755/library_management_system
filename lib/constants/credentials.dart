d46e23ef497f6c4e214a1499a951d7efda618a55
Het Nakhua <nakhuahet@gmail.com>
2024-12-11 13:24:33 +0530

    add more admin users

--- lib/constants/credentials.dart
+++ lib/constants/credentials.dart
@@ -23,5 +23,70 @@ const List<Map<String, String>> credentials = [
     "email": "neha.ram@djsce.ac.in",
     "password":
         "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
-  }
+  },
+  {
+    "email": "vinaya.sawant@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "neha.mendjoge@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "stevina.dias@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "prachi.tawde@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "sharvari.patil@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "sweedle.machado@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "r.sharma@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "priyanca.gonsalves@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "anushree.patkar@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "monali.sankhe@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "savyasaachi.pandit@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "harshal.dalvi@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
+  {
+    "email": "pravin.hole@djsce.ac.in",
+    "password":
+        "\$2a\$10\$cDzIflW97CEmJFwNJFu6e./o9KSnUrPvgMnexsb2Hiv.bRY/USEZC",
+  },
 ];
